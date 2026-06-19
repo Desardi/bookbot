066ad79dc30785bf7ccfed0d2b9f8e858c86a6d9
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-06-19 15:47:41 +0200

    Añadida la funcion para contar palabras

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 def main() -> None:
     ruta = "./books/frankenstein.txt" 
     texto = get_book_text(ruta)
-    print(texto)
+    words = word_count(texto)
+
+    print(f"Found {words} total words")
+
 
 # Filepath es ruta relativa al archivo que queremos pasar
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
-    return f.read()
+        return f.read()
 
 def word_count(text: str) -> int:
     words = text.split()
-    pass
+    return len(words)
 
 main()
