8b7bc555abf920492bebd91bfbb82399cafd7d56
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-06-19 19:07:37 +0200

    Añadida la funcion que cuenta cada uno de los caracteres del libro y lo almacena en un diccionario. Modificada la funcion main para imprimir el diccionario devuelto

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, take_text
 
 def main() -> None:
     ruta = "./books/frankenstein.txt" 
 
     texto = get_book_text(ruta)
     words = get_num_words(texto)
+    num_characters = take_text(texto)
 
     print(f"Found {words} total words")
+    print(num_characters)
+    #for k in num_characters:
+    #    print(f"'{k}': {num_characters[k]}")
+
 
 # Filepath es ruta relativa al archivo que queremos pasar
 def get_book_text(filepath: str) -> str:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words( text: str) -> int:
     words = text.split()
     return len(words)
+
+def take_text(text: str) -> dict[str, int]:
+    characters: dict[str, int] = {}
+    text_lower = text.lower()
+    for character in text_lower:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
