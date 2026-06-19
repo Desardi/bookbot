ff86f1c0f1cc27667f625c8f7ff4629b7b138b55
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-06-19 18:44:16 +0200

    Separado el programa en un archivo nuevo llamado stats.py y main.py. Añadido .gitignore

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+from stats import get_num_words
+
 def main() -> None:
     ruta = "./books/frankenstein.txt" 
+
     texto = get_book_text(ruta)
-    words = word_count(texto)
+    words = get_num_words(texto)
 
     print(f"Found {words} total words")
-
 
 # Filepath es ruta relativa al archivo que queremos pasar
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
         return f.read()
-
-def word_count(text: str) -> int:
-    words = text.split()
-    return len(words)
 
 main()
