19baf2b074f2e494748ef0c268ccb92f2dc100fb
J. Ignacio Montalbán <montardi.juanignacio@gmail.com>
2026-06-21 19:40:00 +0200

    Añadida una funcion que a partir del diccionario, genera una lista y la ordena. Creada una funcion auxiliar para ordenar mediante la funcion sorted.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, take_text
+from stats import get_num_words, take_text, chars_dict_to_sorted_list
 
 def main() -> None:
     ruta = "./books/frankenstein.txt" 
@@ -6,12 +6,12 @@ def main() -> None:
     texto = get_book_text(ruta)
     words = get_num_words(texto)
     num_characters = take_text(texto)
+    count_charachters = chars_dict_to_sorted_list(num_characters)
 
     print(f"Found {words} total words")
-    print(num_characters)
+    print(count_charachters)
     #for k in num_characters:
     #    print(f"'{k}': {num_characters[k]}")
-
 
 # Filepath es ruta relativa al archivo que queremos pasar
 def get_book_text(filepath: str) -> str:
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def take_text(text: str) -> dict[str, int]:
         else:
             characters[character] = 1
     return characters
+
+def sort_on(letra: tuple[str, int]) -> int:
+    return letra[1]
+
+def chars_dict_to_sorted_list(caracteres: dict[str, int]) -> list[tuple[str, int]]:
+    letras: list[tuple[str, int]] = []
+    for k in caracteres:
+        letras.append((k, caracteres[k]))
+    sorted_list = sorted(letras, reverse=True, key=sort_on)
+    return sorted_list
